2e7aa69db9b2e0ad68db279978d8462972484d20
Thijs Brentjens <thijs@brentjensgeoict.nl>
2019-06-05 15:03:53 +0200

    (WIP) prepare for RD CRS

--- src/js/testclient.js
+++ src/js/testclient.js
@@ -13,12 +13,20 @@ function initMap(crsAlias) {
   var baseLayer = {};
   // just to init the map proper
   if (crsAlias=='RD') {
+    // a var set in proj4util.js
+    var RD = new L.Proj.CRS( 'EPSG:28992','+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs',
+        {
+            resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420],
+            bounds: L.bounds([-285401.92, 22598.08], [595401.9199999999, 903401.9199999999]),
+            origin: [-285401.92, 22598.08]
+        }
+    );
     options = {
-      crs: RD, // a var set in proj4util.js
+      crs: RD,
       center: [155000.0, 463000.0],
       zoom: 4,
     }
-    baseLayer = new L.TileLayer('http://geodata.nationaalgeoregister.nl/tms/1.0.0/brtachtergrondkaart/{z}/{x}/{y}.png', {
+    baseLayer = new L.tileLayer('https://geodata.nationaalgeoregister.nl/tms/1.0.0/brtachtergrondkaart/{z}/{x}/{y}.png', {
         minZoom: 0,
         maxZoom: 13,
         tms: true,
@@ -34,8 +42,9 @@ function initMap(crsAlias) {
       });
   }
 
-  map = L.map('map', options);
+  map = new L.map('map', options);
   baseLayer.addTo(map);
+  map.fitBounds(map.getBounds());
   return map;
 }
 
